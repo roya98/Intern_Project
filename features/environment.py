158006d8b8554eff18f0c5e5dec1ca34ece502ab
roya120 <roya.amjaditorshizi@sjsu.edu>
2022-12-19 19:13:16 -0800

    headless and browser

--- features/environment.py
+++ features/environment.py
@@ -9,7 +9,13 @@ def browser_init(context):
     """
     context.driver = webdriver.Chrome(executable_path='/Users/roya/Desktop/Intern_project/chromedriver')
     # context.browser = webdriver.Safari()
-    # context.browser = webdriver.Firefox()
+   # context.driver = webdriver.Firefox(executable_path='/Users/roya/Desktop/Intern_project/geckodriver')
+
+   # options = webdriver.ChromeOptions()
+   # options.add_argument('--headless')
+  #  context.driver = webdriver.Chrome(chrome_options=options,
+                                   #   executable_path='/Users/roya/Desktop/Intern_project/chromedriver')
+
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
